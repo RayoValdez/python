5a5308c6b25f95e32de9c607e87e6e81ff0dc6f9
Catalin <155090297+RayoValdez@users.noreply.github.com>
2024-02-16 18:59:03 +0100

    Update cata.py
    Additional part

--- cata.py
+++ cata.py
@@ -18,3 +18,15 @@ calculate_year_of_birth(name1, age1)
 calculate_year_of_birth(name2, age2)
 calculate_year_of_birth(name3, age3)
 
+# Additional part
+def calculate_year_of_birth_v2(name, age, target_age):
+    current_year = 2024
+
+    birth_year = current_year - age + target_age
+
+    print(f"{name} will turn {target_age} years old in the year {birth_year}")
+
+target_age = 100
+calculate_year_of_birth_v2(name1, age1, target_age)
+calculate_year_of_birth_v2(name2, age2, target_age)
+calculate_year_of_birth_v2(name3, age3, target_age)
